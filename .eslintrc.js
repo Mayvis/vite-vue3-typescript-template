28ed16afd53052f9c79f128f4691d5f97c69d386
Mayvis <kevin73911@gmail.com>
2022-05-21 11:21:45 +0800

    setup eslint and prettier with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    'prettier'
+  ],
+  plugins: ['import'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'import/no-unresolved': 'error',
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  }
+}
